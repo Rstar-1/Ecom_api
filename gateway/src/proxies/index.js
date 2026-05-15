90345367fc06163a0396770dd39bd42c0d3922f4
rajs <raj.shetye@sequretek.com>
2026-05-15 22:20:09 +0530

    temp commit

---
+++ gateway/src/proxies/index.js
@@ -0,0 +1,9 @@
+import authProxy from "./auth.proxy.js";
+import userProxy from "./user.proxy.js";
+import productProxy from "./product.proxy.js";
+import cartProxy from "./cart.proxy.js";
+import orderProxy from "./order.proxy.js";
+import paymentProxy from "./payment.proxy.js";
+import roleProxy from "./role.proxy.js";
+
+export { authProxy, userProxy, productProxy, cartProxy, orderProxy, paymentProxy, roleProxy };
---
+++ shared/middlewares/error.middleware.js
@@ -0,0 +1,32 @@
+import { constants } from "../index.js";
+const { statusCodes, messages } = constants;
+
+export const errorMiddleware = (err, req, res, next) => {
+  console.error("❌ Error:", err);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  /* ================= MULTER ERROR (NO IMPORT) ================= */
+  if (err.name === "MulterError") {
+    return res.status(statusCodes.HTTP_STATUS.BAD_REQUEST).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  /* ================= FILE TYPE ERROR ================= */
+  if (err.message === "Only JPG, PNG, PDF allowed") {
+    return res.status(statusCodes.HTTP_STATUS.BAD_REQUEST).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  /* ================= DEFAULT ================= */
+  res.status(err.statusCode || statusCodes.HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: err.message || messages.INTERNAL_SERVER_ERROR,
+  });
+};
